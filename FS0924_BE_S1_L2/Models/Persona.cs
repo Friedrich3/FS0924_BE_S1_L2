6ffb18309dfb8563d556984395cdacbd1394c69f
Federico Tonti <148573975+Friedrich3@users.noreply.github.com>
2025-02-11 15:41:26 +0100

    ESERCIZIO ODIERNO

---
+++ FS0924_BE_S1_L2/Models/Persona.cs
@@ -0,0 +1,43 @@
+﻿namespace FS0924_BE_S1_L2.Models
+{
+    internal class Persona
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Eta {  get; set; }
+        
+        public string GetNome()
+        {
+            return Nome;
+        }
+        public string GetCognome()
+        {
+            return Cognome;
+        }
+        public string GetEta()
+        {
+            return Eta.ToString();
+        }
+
+        public string GetDettagli()
+        {
+            return $"Nome:{Nome}, Cognome:{Cognome}, Eta': {Eta}";
+        }
+
+        public Persona(string nome, string cognome)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            Eta = "Non Specificata";
+
+        }
+        public Persona(string nome, string cognome, int eta) 
+        { 
+            Nome=nome;
+            Cognome = cognome;
+            Eta = eta.ToString();
+        }
+
+
+    }
+}
--- FS0924_BE_S1_L2/Program.cs
+++ FS0924_BE_S1_L2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FS0924_BE_S1_L2.Models;
+
+
+var persona1 = new Persona("Federico","Tonti",29);
+var persona2 = new Persona("Vittorio", "Turiaci");
+
+
+
+
+
+Console.WriteLine(persona1.GetNome());
+Console.WriteLine(persona1.GetCognome());
+Console.WriteLine(persona1.GetEta());
+Console.WriteLine(persona1.GetDettagli());
+Console.WriteLine(persona2.GetNome());
+Console.WriteLine(persona2.GetCognome());
+Console.WriteLine(persona2.GetEta());
+Console.WriteLine(persona2.GetDettagli());
